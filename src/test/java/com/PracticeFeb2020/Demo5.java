9fa2789b94a255e889116a827320dd8f3148816b
daryal89 <creativity80@gmail.com>
2020-02-06 15:46:08 -0500

    added features--in pom

---
+++ src/test/java/com/PracticeFeb2020/Demo5.java
@@ -0,0 +1,25 @@
+package com.PracticeFeb2020;
+
+import org.testng.annotations.Test;
+
+public class Demo5 {
+
+	@Test
+	public void test5() {
+		System.out.println("Appium Practice");
+
+	}
+
+	@Test
+	public void test6() {
+		System.out.println("RestApi Practice!!!");
+
+	}
+
+	@Test
+	public void test7() {
+		System.out.println("Cucumber Practice!!!");
+
+	}
+
+}
