d067d971f942e78193915c8a8f6c9d8abef3d0a5
daryal89 <creativity80@gmail.com>
2020-02-06 14:40:17 -0500

    added methods

---
+++ src/main/java/com/PracticeFeb2020/Demo3.java
@@ -0,0 +1,10 @@
+package com.PracticeFeb2020;
+
+public class Demo3 {
+
+	public static void main(String[] args) {
+		System.out.println("Selenium code added by dhruba Aryal");
+
+	}
+
+}
--- src/main/java/com/PracticeFeb2020/Demo4.java
+++ src/main/java/com/PracticeFeb2020/Demo4.java
@@ -1,9 +1,10 @@
 package com.PracticeFeb2020;
 
-public class Demo2 {
+public class Demo4 {
 
 	public static void main(String[] args) {
-		System.out.println("Selenium Practice!!!");
+		System.out.println("Git code added!!!");
 
 	}
+
 }
